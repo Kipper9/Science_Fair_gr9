8dbf0afe0f00fe7d174c17c1d3b82e7c8d066437
Kipp Puffalt <puffaltk@gmail.com>
2023-11-09 14:48:44 -0700

    finished the point checks

--- Dots&Boxes.py
+++ Dots&Boxes.py
@@ -27,27 +27,39 @@ def click(row, col, turn):
   Grid[row][col] = turn
   
   if len(Grid[row]) == 4:
-    if Grid[row][col] == turn and Grid[row-1][col] == turn and Grid[row-1][col] == turn and Grid[row-2][col] == turn:
-      Team_points[turn] =+ 1
-      print("1 point for team",turn)
+    if Grid[row-1][col] != 0 and Grid[row-1][col+1] != 0 and Grid[row-2][col] != 0:
+      Team_points[turn - 1] =+ 1
+      print("1 point for team 1")
   
-    if Grid[row][col] == turn and Grid[row+1][col] == turn and Grid[row+1][col+1] == turn and Grid[row+2][col] == turn:
-      Team_points[turn] =+ 1
-      print("1 point for team",turn)
+    if Grid[row+1][col] != 0 and Grid[row+1][col+1] != 0 and Grid[row+2][col] != 0:
+      Team_points[turn - 1] =+ 1
+      print("1 point for team 2")
   
-  # if len(Grid[row]) == 5:
-  #   if Grid[row][col] == turn and Grid[row][col] == turn and Grid[row][col] == turn and Grid[row][col] == turn:
-  #     Team_points[turn] =+ 1
-  #     print("1 point for team",turn)
-  
-  #   if Grid[row][col] == turn and Grid[row][col] == turn and Grid[row][col] == turn and Grid[row][col] == turn:
-  #     Team_points[turn] =+ 1
-  #     print("1 point for team",turn)
+  if len(Grid[row]) == 5:
+    if Grid[row][col-1] != 0 and Grid[row-1][col-1] != 0 and Grid[row+1][col-1] != 0:
+      Team_points[turn - 1] =+ 1
+      print("1 point for team 3")
 
-click(2,1,1)
-click(2,2,1)
-click(3,1,1)
-click(1,1,1)
+    if Grid[row-1][col] != 0 and Grid[row+1][col] != 0 and Grid[row][col+1] != 0:
+      Team_points[turn - 1] =+ 1
+      print("1 point for team 4")
 
+
+click(0,0,1)
+for x in range(0,9):
+  print(Grid[x])
+print("")
+
+click(1,0,1)
+for x in range(0,9):
+  print(Grid[x])
+print("")
+
+click(2,0,1)
+for x in range(0,9):
+  print(Grid[x])
+print("")
+
+click(1,1,2)
 for x in range(0,9):
   print(Grid[x])

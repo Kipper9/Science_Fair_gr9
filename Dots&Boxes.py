61f5c0634f8e4c0d4457d07920991dd9597e5800
Kipp Puffalt <puffaltk@gmail.com>
2023-12-05 21:04:01 -0700

    changed dot and boxes into a class for ml

--- Dots&Boxes.py
+++ Dots&Boxes.py
@@ -1,104 +1,83 @@
-Turn_over = False
-Team_points = [0,0]
-completeSquares = 0
-Grid = []
-def MakeGrid(R,C):
-  global Grid
-  for i in range(0,R):
-    Grid.append([])
-    if i%2 == 0:
-      for x in range(0,C-1):
-        Grid[i].append(0)
+class DotsAndBoxes:
+  def __init__(self,move,gameSate,turn):
+    self.grid = gameSate
+    self.turn = turn
+    self.completeSquares = 0
+    self.points = [0,0]
+    self.rows = len(gameSate)
+    self.cols = len(gameSate[1])
+    self.isTurnOver = False
+    self.move = move
+    self.gameState = gameSate
+
+  def makeGrid(self):
+    grid = []
+    for i in range(0,self.rows):
+      grid.append([])
+      if i%2 == 0:
+        for x in range(0,self.cols-1):
+          self.grid[i].append(0)
+      else:
+        for x in range(0,self.cols):
+          grid[i].append(0)
+    return grid
+
+  def isGameOver(self):
+    if self.completeSquares == (self.rows - 1) * (self.cols - 1):
+      return True
     else:
-      for x in range(0,C):
-        Grid[i].append(0)
-  return Grid
-def game_over(m):
-  global Turn_over
-  global completeSquares
-  global Grid
-  if completeSquares == m:
-    Turn_over = True
-    return True
-  else:
-    return False
-def draw_Board():
-  print("Board:")
-  for i in Grid:
-    print(i)
-def click(row, col, turn,r,c):
-  global completeSquares
-  global Turn_over
-  global Grid
-  global Team_points
-  if row > r+1 or col >= len(Grid[row]):
-    print('Invalid move')
-    Turn_over = False 
-  elif Grid[row][col] == 1 or Grid[row][col] == 2:
-    print("There is already a line there.")
-    Turn_over = False
-  else:
-    Grid[row][col] = turn
-    Turn_over = True
-    if len(Grid[row]) == c-1:
-      if row != 0:
-        if Grid[row-1][col] != 0 and Grid[row-1][col+1] != 0 and Grid[row-2][col] != 0:
-          Team_points[turn - 1] += 1
-          print("1 point for team", turn)
-          completeSquares += 1
-          Turn_over = False
-      if row < r-1:
-        if Grid[row+1][col] != 0 and Grid[row+1][col+1] != 0 and Grid[row+2][col] != 0:
-          Team_points[turn - 1] += 1
-          print("1 point for team", turn)
-          completeSquares += 1
-          Turn_over = False
-    if len(Grid[row]) == c:
-      if col != 0:
-        if Grid[row][col-1] != 0 and Grid[row-1][col-1] != 0 and Grid[row+1][col-1] != 0:
-          Team_points[turn - 1] += 1
-          print("1 point for team",turn)
-          completeSquares += 1
-          Turn_over = False
-      if col < c-1:
-        if Grid[row-1][col] != 0 and Grid[row+1][col] != 0 and Grid[row][col+1] != 0:
-          Team_points[turn - 1] += 1
-          print("1 point for team", turn)
-          completeSquares += 1
-          Turn_over = False
-def play():
-  global Grid
-  global Team_points
-  global Turn_over
-  Rows = int(input('How many rows: '))
-  Cols = int(input('How many Columns: '))
-  MaxSquares = (Rows-1)*(Cols-1)
-  Rows = (Rows - 1) * 2 + 1
-  Grid = MakeGrid(Rows,Cols)
-  Turn = 1
-  while not game_over(MaxSquares):
-    while Turn_over == False:
-        print()
-        print("Player",Turn,"'s move")
-        print()
-        r = int(input("What row would you like to play: ")) - 1
-        c = int(input("What column would you like to play: ")) - 1
-        click(r,c,Turn,Rows,Cols)
-        print()
-        draw_Board()
-        if game_over(MaxSquares):
-          Turn_over = True
-    Turn_over = False
-    Turn = -1 * Turn + 3
-  print()
-  print("Game over")
-  print()
-  if Team_points[1] > Team_points[0]:
-    print("Team 2 wins!")
-  elif Team_points[1] < Team_points[0]:
-    print("Team 1 wins!") 
-  elif Team_points[1] == Team_points[0]:
-    print("Tie, no one wins :(")
-  for i in range(0,2):
-    print('Team',i+1,'got',Team_points[i],'points')
-play()
+      return False
+
+  def draw_Board(self):
+    print("Board:")
+    for i in self.grid:
+      print(i)
+
+  def click(self, row, col):
+    
+    if row > self.rows + 1 or col >= len(self.grid[row]):
+      print('Invalid move')
+      self.isTurnOver = False 
+
+    elif self.grid[row][col] == 1 or self.grid[row][col] == 2:
+      print("There is already a line there.")
+      self.isTurnOver = False
+
+    else:
+      self.grid[row][col] = self.turn
+      self.isTurnOver = True
+      if len(self.grid[row]) == self.cols-1:
+        if row != 0:
+          if self.grid[row-1][col] != 0 and self.grid[row-1][col+1] != 0 and self.grid[row-2][col] != 0:
+            self.points[self.turn - 1] += 1
+            print("1 point for team", self.turn)
+            self.completeSquares += 1
+            self.isTurnOver = False
+
+        if row < self.rows-1:
+          if self.grid[row+1][col] != 0 and self.grid[row+1][col+1] != 0 and self.grid[row+2][col] != 0:
+            self.points[self.turn - 1] += 1
+            print("1 point for team", self.turn)
+            self.completeSquares += 1
+            self.isTurnOver = False
+
+      if len(self.grid[row]) == self.cols:
+        if col != 0:
+          if self.grid[row][col-1] != 0 and self.grid[row-1][col-1] != 0 and self.grid[row+1][col-1] != 0:
+            self.points[self.turn - 1] += 1
+            print("1 point for team",self.turn)
+            self.completeSquares += 1
+            self.isTurnOver = False
+
+        if col < self.cols-1:
+          if self.grid[row-1][col] != 0 and self.grid[row+1][col] != 0 and self.grid[row][col+1] != 0:
+            self.points[self.turn - 1] += 1
+            print("1 point for team", self.turn)
+            self.completeSquares += 1
+            self.isTurnOver = False
+
+  def play(self):
+    self.click(self.move[0] - 1,self.move[1] - 1)
+    self.draw_Board()
+    return self.gameState, self.points, self.isTurnOver, self.isGameOver()
+    

da333e2ab860feb6c8bc70864ce3dbaa6ffd9169
Kipp Puffalt <puffaltk@gmail.com>
2023-12-11 19:08:52 -0700

    changed name and made more AI friendly

--- DotsAndBoxes.py
+++ DotsAndBoxes.py
@@ -1,13 +1,11 @@
 class DotsAndBoxes:
-  def __init__(self,move,gameSate,turn):
+  def __init__(self,gameSate):
     self.grid = gameSate
-    self.turn = turn
     self.completeSquares = 0
     self.points = [0,0]
     self.rows = len(gameSate)
     self.cols = len(gameSate[1])
     self.isTurnOver = False
-    self.move = move
     self.gameState = gameSate
 
   def makeGrid(self):
@@ -29,11 +27,12 @@ class DotsAndBoxes:
       return False
 
   def draw_Board(self):
+    print()
     print("Board:")
     for i in self.grid:
       print(i)
 
-  def click(self, row, col):
+  def click(self, row, col,turn):
     
     if row > self.rows + 1 or col >= len(self.grid[row]):
       print('Invalid move')
@@ -44,40 +43,50 @@ class DotsAndBoxes:
       self.isTurnOver = False
 
     else:
-      self.grid[row][col] = self.turn
+      self.grid[row][col] = turn
+      self.draw_Board()
       self.isTurnOver = True
       if len(self.grid[row]) == self.cols-1:
         if row != 0:
           if self.grid[row-1][col] != 0 and self.grid[row-1][col+1] != 0 and self.grid[row-2][col] != 0:
-            self.points[self.turn - 1] += 1
-            print("1 point for team", self.turn)
+            self.points[turn - 1] += 1
+            print("1 point for team", turn)
             self.completeSquares += 1
             self.isTurnOver = False
 
         if row < self.rows-1:
           if self.grid[row+1][col] != 0 and self.grid[row+1][col+1] != 0 and self.grid[row+2][col] != 0:
-            self.points[self.turn - 1] += 1
-            print("1 point for team", self.turn)
+            self.points[turn - 1] += 1
+            print("1 point for team", turn)
             self.completeSquares += 1
             self.isTurnOver = False
 
       if len(self.grid[row]) == self.cols:
         if col != 0:
           if self.grid[row][col-1] != 0 and self.grid[row-1][col-1] != 0 and self.grid[row+1][col-1] != 0:
-            self.points[self.turn - 1] += 1
-            print("1 point for team",self.turn)
+            self.points[turn - 1] += 1
+            print("1 point for team",turn)
             self.completeSquares += 1
             self.isTurnOver = False
 
         if col < self.cols-1:
           if self.grid[row-1][col] != 0 and self.grid[row+1][col] != 0 and self.grid[row][col+1] != 0:
-            self.points[self.turn - 1] += 1
-            print("1 point for team", self.turn)
+            self.points[turn - 1] += 1
+            print("1 point for team", turn)
             self.completeSquares += 1
             self.isTurnOver = False
 
-  def play(self):
-    self.click(self.move[0] - 1,self.move[1] - 1)
-    self.draw_Board()
-    return self.gameState, self.points, self.isTurnOver, self.isGameOver()
-    
+  def play(self,action,turn):
+    self.click(action[0],action[1],turn)
+    return self.points, self.isTurnOver, self.isGameOver()
+
+game = DotsAndBoxes([[0,0],[0,0,0],[0,0]])
+
+state = game.play([0,0],1)
+print(state)
+state = game.play([1,1],2)
+print(state)
+state = game.play([1,0],1)
+print(state)
+state = game.play([2,0],2)
+print(state)
